2ec0532aa99b1dc58248aff5d3125244b3c8c7ed
Nikola Saratlija <saratlijanikola@gmail.com>
2022-07-11 15:31:11 +0200

    added frontend

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,23 @@
-@extends('layout')
+<x-layout>
+    @include('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card :post="$posts[0]"/>
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+</x-layout>
+
+{{-- @extends('layout')
 
 @section('banner')
     <h1>My Blog</h1>
@@ -21,4 +40,4 @@
             <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
-@endsection
+@endsection --}}
