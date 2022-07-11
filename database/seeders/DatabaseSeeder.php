9daaa8620de3ba93e185cdbd19fe2d0105ee89e8
Nikola Saratlija <saratlijanikola@gmail.com>
2022-07-11 13:51:39 +0200

    postfactory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,52 +17,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        User::factory(2)->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        $user = User::factory()->create([
+            'name' => 'John Doe'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        Post::factory(2)->create([
+            'user_id' => $user->id
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-
-        Post::create([
-            'title' => 'First Post',
-            'slug' => 'first-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni reiciendis hic corrupti necessitatibus, alias beatae perspiciatis quibusdam iure distinctio quis nisi et dolor, itaque, expedita minus consectetur a. Consectetur eos delectus enim aliquam animi aut incidunt laboriosam non excepturi repudiandae esse, commodi, eligendi quibusdam quos accusamus cupiditate illum iusto optio?',
-            'category_id' => $family->id,
-            'user_id' => User::find(1)->id
-        ]);
-
-        Post::create([
-            'title' => 'Second Post',
-            'slug' => 'second-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni reiciendis hic corrupti necessitatibus, alias beatae perspiciatis quibusdam iure distinctio quis nisi et dolor, itaque, expedita minus consectetur a. Consectetur eos delectus enim aliquam animi aut incidunt laboriosam non excepturi repudiandae esse, commodi, eligendi quibusdam quos accusamus cupiditate illum iusto optio?',
-            'category_id' => $personal->id,
-            'user_id' => User::find(2)->id
-        ]);
-
-        Post::create([
-            'title' => 'Third Post',
-            'slug' => 'third-post',
-            'excerpt' => 'Lorem ipsum dolor sit amet.',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni reiciendis hic corrupti necessitatibus, alias beatae perspiciatis quibusdam iure distinctio quis nisi et dolor, itaque, expedita minus consectetur a. Consectetur eos delectus enim aliquam animi aut incidunt laboriosam non excepturi repudiandae esse, commodi, eligendi quibusdam quos accusamus cupiditate illum iusto optio?',
-            'category_id' => $work->id,
-            'user_id' => User::find(2)->id
-        ]);
+        Post::factory(3)->create();
     }
 }
