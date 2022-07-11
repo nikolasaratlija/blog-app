01f7a88eaf5cac5c647fe2116296bbcbcd5eaafd
Nikola Saratlija <saratlijanikola@gmail.com>
2022-07-11 12:20:10 +0200

    post slug url

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,13 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('posts', ['posts' => Post::all()]);
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
 
-Route::get('posts/{post}', function ($id) {
-    return view(
-        'post',
-        ['post' => Post::find($id)]
-    );
+Route::get('posts/{post:slug}', function (Post $post) {
+    return view('post', [
+        'post' => $post
+    ]);
 });
