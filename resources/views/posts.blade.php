291d82a2340bca47aec282d1eb64ef5f6940be8a
Nikola Saratlija <saratlijanikola@gmail.com>
2022-07-11 16:31:12 +0200

    dynamic post page

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,42 +2,10 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post="$posts[0]"/>
-
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card />
-            <x-post-card />
-        </div>
-
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-        </div>
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts"/>
+        @else
+            <p>No posts yet. Please check back later.</p>
+        @endif
     </main>
 </x-layout>
-
-{{-- @extends('layout')
-
-@section('banner')
-    <h1>My Blog</h1>
-@endsection
-
-@section('content')
-    @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-
-            <p>
-                By <a href="/authors/{{ $post->author->username }}"> {{ $post->author->name }}</a> in <a
-                    href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-
-            <p>{{ $post->excerpt }}</p>
-        </article>
-    @endforeach
-@endsection --}}
