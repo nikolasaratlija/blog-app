1b3ba51275ed617dfb23db74affa927909165793
Nikola Saratlija <saratlijanikola@gmail.com>
2022-07-13 00:16:14 +0200

    author search fix

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,13 +11,13 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category']))->get()
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get()
         ]);
     }
 
     public function show(Post $post)
     {
-        return view('post.show', [
+        return view('post', [
             'post' => $post
         ]);
     }
--- resources/views/components/posts/card-featured.blade.php
+++ resources/views/components/posts/card-featured.blade.php
@@ -37,7 +37,7 @@
                 <div class="flex items-center text-sm">
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <h5 class="font-bold">
-                        <a href="/?authors={{ $post->author->username }}">{{ $post->author->name }}</a>
+                        <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
                     </h5>
                 </div>
 
