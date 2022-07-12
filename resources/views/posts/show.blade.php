38d7d4bc6a322d2cbebb4e9eae7bc814f71d3726
Nikola Saratlija <saratlijanikola@gmail.com>
2022-07-13 00:14:33 +0200

    author search fix

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,7 +14,7 @@
                     <div class="ml-3 text-left">
                         <a href="/authors/{{ $post->author->username }}"></a>
                         <h5 class="font-bold">
-                            <a href="/?authors={{ $post->author->username }}">{{ $post->author->name }}</a>
+                            <a href="/?author={{ $post->author->username }}">{{ $post->author->name }}</a>
                         </h5>
                     </div>
                 </div>
