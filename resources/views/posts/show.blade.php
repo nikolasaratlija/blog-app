51a47db622bc96c84fccb72b8be92f7079e4cb9b
Nikola Saratlija <saratlijanikola@gmail.com>
2022-07-13 17:15:26 +0200

    cdn for avatars

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,7 +10,7 @@
                 </p>
 
                 <div class="flex items-center lg:justify-center text-sm mt-4">
-                    <img src="/images/lary-avatar.svg" alt="Lary avatar">
+                    <img src="https://i.pravatar.cc/55" alt="Lary avatar">
                     <div class="ml-3 text-left">
                         <a href="/authors/{{ $post->author->username }}"></a>
                         <h5 class="font-bold">
