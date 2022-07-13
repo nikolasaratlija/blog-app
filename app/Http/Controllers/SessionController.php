d05ebfe388ad285d6df84887c72a8b4841b39f2a
Nikola Saratlija <saratlijanikola@gmail.com>
2022-07-13 17:04:43 +0200

    login page

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class SessionController extends Controller
 {
@@ -11,5 +12,24 @@ class SessionController extends Controller
         auth()->logout();
 
         return redirect('/')->with('success', 'You have been logged out.');
+    }
+
+    public function create()
+    {
+        return view('login.create');
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => ['required', 'email', 'max:255'],
+            'password' => ['required', 'max:255']
+        ]);
+
+        if (auth()->attempt($attributes)) {
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        throw ValidationException::withMessages(['email' => 'Incorrect email or password']);
     }
 }
